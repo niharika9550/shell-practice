234c9c62bebcb85dd64edf2dd855aeed58b263ee
niharika9550 <niharikaguduruaws@gmail.com>
2026-02-02 10:46:18 +0530

    kjfhgdu

---
+++ conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "suresh: :hi ramesh how are you"
+echo "ramesh: :hi suresh iam fine how are you doing"
+echo "suresh: :doing fine what are you learning now"
+echo "ramesh: :i am learning devops"
