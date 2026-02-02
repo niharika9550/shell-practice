fe2366c3cc75465c007bbf1b97c3f015d0cd3e6a
niharika9550 <niharikaguduruaws@gmail.com>
2026-02-02 16:51:47 +0530

    fg

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NUMBER=$1
 if [ $NUMBER -gt 20 ]; then
-echo "given number is greater than 20"
+    echo "given number:$NUMBER  is greater than 20"
 elif [ $NUMBER -eq 20 ]; then 
-echo "given number is equal to 20"
+    echo "given number:$NUMBER  is equal to 20"
 else
-echo "given number is less than 20"
+echo "given number :$NUMBER  is less than 20"
