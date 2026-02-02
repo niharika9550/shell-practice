54ca1258b14d54c4a52a84e4dade93f091dd451b
niharika9550 <niharikaguduruaws@gmail.com>
2026-02-02 15:33:24 +0530

    fg

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 echo "script executed at: $TIMESTAMP"
+sleep 10
+END _TIME=$(date +%s)
+TOTAL_TIME=$((END_TIME -$START_TIME))
+echo "script executed in : $TOTAL_TIME"
