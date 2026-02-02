447e8758b812918c3e74cbb2f2fa2c208138eecb
niharika9550 <niharikaguduruaws@gmail.com>
2026-02-02 15:55:37 +0530

    fg

---
+++ 07-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "all args passed to script : $@"
+echo echo "no of vars passed to script $#"
+echo "script name:$0"
+echo "present directory :$pwd"
+echo "who is running :$USER"
+echo "home directory of the current user :$HOME"
+echo "PID of the script :$$"
+sleeo 100&
+echo "PID of recently executed script :$!"
+echo "all args passed to script :$@"
