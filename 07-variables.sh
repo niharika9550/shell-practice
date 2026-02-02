8610f638736cf5d3cec39c1bcbd1669ee52c369e
niharika9550 <niharikaguduruaws@gmail.com>
2026-02-02 15:57:34 +0530

    fg

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "all args passed to script : $@"
-echo echo "no of vars passed to script $#"
+echo "no of vars passed to script $#"
 echo "script name:$0"
 echo "present directory :$pwd"
 echo "who is running :$USER"
