ac025ad72747a16d5f909343cabc303d3fd11c43
niharika9550 <niharikaguduruaws@gmail.com>
2026-02-02 12:48:22 +0530

    df

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "ramesh : :hi suresh how are you"
+echo "suresh : :hi ramesh iam fine how are you doing"
+echo "ramesh : :i am doing grat what are you learning"
+echo "suresh : : iam learning devops"
