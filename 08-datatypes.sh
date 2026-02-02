e6736485b30bc3d843ce16c842272111b42d674b
niharika9550 <niharikaguduruaws@gmail.com>
2026-02-02 16:26:45 +0530

    fg

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -3,3 +3,9 @@ NUM1=100
 NUM2=siva
 SUM=$(($NUM1+$NUM2))
 echo "SUM=$SUM"
+#array
+FRUITS=("Apple","mango" "banana")
+echo "fruit are :${fruits[@]}"
+echo "first fruit is: ${FRUITS[0]}"
+echo "second fruit is :${FRUITS[1]}"
+echo  "thir fruit is :{FRUITS[2]}"
