6f13f7f63779af70e3e5a8c2fb98bc4b70b52aed
niharika9550 <niharikaguduruaws@gmail.com>
2026-02-02 13:05:15 +0530

    fdffn

---
+++ 03-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "PERSON1 : :hi PERSON2 how are you"
+echo "PERSON2 : :hi PERSON1 iam fine how are you doing"
+echo "PERSON1 : :i am doing grat what are you learning"
+echo "PERSON2 : : iam learning devops"
